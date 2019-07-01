e328bc61274df736ea29740e5f7005c147218235
duyh09 <47871263+duyh09@users.noreply.github.com>
2019-06-30 20:11:56 -0400

    Create netmikoconfig.py

---
+++ netmikoconfig.py
@@ -0,0 +1,37 @@
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetMikoTimeoutException
+from paramiko.ssh_exception import SSHException
+from netmiko.ssh_exception import AuthenticationException
+with open('prelist') as f:
+    commands_list = f.read().splitlines()
+
+with open('r5r6') as f:
+    devices_list = f.read().splitlines()
+
+for devices in devices_list:
+    print ("connecting to device" + str(devices))
+    ip_address_of_device = devices
+    ios_device = {
+        'device_type': 'cisco_ios',
+        'ip': ip_address_of_device,
+        'username': 'cisco',
+        'password': 'cisco'
+    }
+
+    try:
+        net_connect = ConnectHandler(**ios_device)
+    except (AuthenticationException):
+        print ("AUTH FAIL " + ip_address_of_device)
+        continue
+    except (NetMikoTimeoutException):
+        print ("Time out " + ip_address_of_device)
+        continue
+    except (SSHException):
+        print (" SSH ISSUE " + ip_address_of_device)
+        continue
+
+
+    output = net_connect.send_config_set(commands_list)
+    print (output)
+    output = net_connect.send_command('clear ip bgp * soft')
+    print (output)
